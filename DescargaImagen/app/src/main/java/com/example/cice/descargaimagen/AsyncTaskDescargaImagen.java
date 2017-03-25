0cd63b7cf4bb235b7176ed921acff50bcf481e03
cice <cice@aula8PovMacN08.local>
2017-03-25 12:11:44 +0100

    descargar varias imagenes

--- DescargaImagen/app/src/main/java/com/example/cice/descargaimagen/AsyncTaskDescargaImagen.java
+++ DescargaImagen/app/src/main/java/com/example/cice/descargaimagen/AsyncTaskDescargaImagen.java
@@ -13,7 +13,7 @@ import java.net.URL;
  * Created by cice on 25/3/17.
  */
 
-public class AsyncTaskDescargaImagen extends AsyncTask<String,Void,Bitmap> {
+public class AsyncTaskDescargaImagen extends AsyncTask<String,Void,Bitmap[]> {
 
     private MainActivity mainActivity;
 
@@ -24,9 +24,10 @@ public class AsyncTaskDescargaImagen extends AsyncTask<String,Void,Bitmap> {
 
 
     @Override
-    protected Bitmap doInBackground(String... url) {
+    protected Bitmap[] doInBackground(String... url) {
 
         //VARARGS STRING ... string -> número indeterminado de  parámetros. de tipo String.
+        Bitmap[] listaBitmap=null;
         Bitmap bitmap=null;
         String url_imagen=null;
         //Cogemos la ruta de la foto.
@@ -38,37 +39,51 @@ public class AsyncTaskDescargaImagen extends AsyncTask<String,Void,Bitmap> {
         //Clase para recibir la foto;
         InputStream is=null;
 
-        try {
-            url_imagen=url[0];
-            //Construimos la URL.
-            url_foto = new URL(url_imagen);
-            //Abrimos la conexion y por defecto se llama al método GET
-            http=(HttpURLConnection)url_foto.openConnection();
-            //http.setRequestMethod("GET") así especificamos el método HTTP a ejecutar;
-            respuesta=http.getResponseCode();
-            //Comprobamos si la petición fue correcta
-            if(respuesta== HttpURLConnection.HTTP_OK){
-                Log.d("MENSAJE","La petición se ha procesado correctamente");
-                //Accedo al cuerpo del mensaje HTTP.
-                is=http.getInputStream();
-                bitmap=BitmapFactory.decodeStream(is);
-            }else{
-                Log.e("MENSAJE","La petición fue mal. No hay foto");
 
+
+            listaBitmap = new Bitmap[url.length];
+
+            try{
+             for (int i = 0; i < url.length; i++) {
+
+                    url_imagen = url[i];
+                    //Construimos la URL.
+                    url_foto = new URL(url_imagen);
+                    //Abrimos la conexion y por defecto se llama al método GET
+                    http = (HttpURLConnection) url_foto.openConnection();
+                    //http.setRequestMethod("GET") así especificamos el método HTTP a ejecutar;
+                    respuesta = http.getResponseCode();
+                    //Comprobamos si la petición fue correcta
+                    if (respuesta == HttpURLConnection.HTTP_OK) {
+                        Log.d("MENSAJE", "La petición se ha procesado correctamente");
+                        //Accedo al cuerpo del mensaje HTTP.
+                        is = http.getInputStream();
+                        bitmap = BitmapFactory.decodeStream(is);
+                        listaBitmap[i] = bitmap;
+
+                    } else {
+                        Log.e("MENSAJE", "La petición fue mal. No hay foto");
+
+                    }
+
+                }
+
+
+            } catch (Throwable t) {
+                Log.e("MENSAJE", "Algo fue mal", t);
+
+            } finally {
+                //Liberamos la conexión tanto si fue mal o no.
+                http.disconnect();
             }
-        }catch(Throwable t){
-            Log.e("MENSAJE","Algo fue mal",t);
 
-        }finally{
-            //Liberamos la conexión tanto si fue mal o no.
-            http.disconnect();
-        }
 
-        return bitmap;
+
+        return listaBitmap;
     }
 
     @Override
-    protected void onPostExecute(Bitmap bitmap) {
+    protected void onPostExecute(Bitmap[] bitmap) {
         Log.d("MENSAJE","Tarea de descarga finalizada");
         mainActivity.dibujarImagen(bitmap);
 
--- DescargaImagen/app/src/main/java/com/example/cice/descargaimagen/MainActivity.java
+++ DescargaImagen/app/src/main/java/com/example/cice/descargaimagen/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     //URL de la imagen a descargar de Internet en el dispositivo móvil.
     private static final String URL_IMAGEN="http://images.performgroup.com/di/library/goal_de/76/43/antoine-griezmann-atletico-madrid-champions-league-03052016_1fl7g2ws0nyj16hewzbt5lgnl.jpg";
-
+    //URL de la imagen 2.
+    private static final String URL_IMAGEN2="http://estaticos.marca.com/imagenes/2015/01/18/futbol/equipos/atletico/1421611936_extras_noticia_foton_7_1.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,24 +26,32 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MENSAJE","Tocó el botón de descargar");
         AsyncTaskDescargaImagen atdi=new AsyncTaskDescargaImagen(this);
         //Llamada al servicio de descarga de la imagen y llama al método doInBackgroud.
-        atdi.execute(URL_IMAGEN);
+        atdi.execute(URL_IMAGEN,URL_IMAGEN2);
         Log.d("MENSAJE","Tarea descarga lanzada");
         //Bloqueamos el boton para impedir que se puede pulsar múltiples veces.
         view.setEnabled(false);
     }
 
 
-    public void dibujarImagen(Bitmap bitmap){
+    public void dibujarImagen(Bitmap[] listaBitmap) {
 
-        Log.d("MENSAJE","Dibujamos la imagen descargada en el Image View");
-        //Pintamos la imagen encapsula en el bitmap.
-        ImageView imageView=(ImageView)findViewById(R.id.miImagen);
-        imageView.setImageBitmap(bitmap);
-        Log.d("MENSAJE","Imagen seteada");
-        Button botonDescargar=(Button)findViewById(R.id.botondescarga);
+        Log.d("MENSAJE", "Dibujamos la imagen descargada en el Image View");
+        Button botonDescargar = (Button) findViewById(R.id.botondescarga);
         //Desbloqueamos el botón una vez descargada la imagen.
         botonDescargar.setEnabled(true);
-    }
+        ImageView imageView=null;
+        for (int i = 0; i < listaBitmap.length; i++) {
 
+            //Pintamos la imagen encapsula en el bitmap.
+            //Construimos los id de forma dinámica (parte comun, destino,paquete de la clase).
+            int id=getResources().getIdentifier("miImagen"+i,"id","com.example.cice.descargaimagen");
+            imageView = (ImageView) findViewById(id);
+            Bitmap bitmap=listaBitmap[i];
+            imageView.setImageBitmap(bitmap);
+            Log.d("MENSAJE", "Imagen seteada");
+
+        }
+
+    }
 
 }
